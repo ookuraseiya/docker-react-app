cc42366288b09dd60adb74c630febbfd24a137a8
ookuraseiya <seiya.ookura@gmail.com>
2023-03-16 17:17:58 +0900

    2023/3/16 git commit test

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './css/styles.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -24,5 +24,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
